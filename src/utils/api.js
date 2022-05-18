be9815d4b1f0240d8ac78c39831f76bdbcdf3ba8
吴闯 <wuchuang456@126.com>
2022-05-18 17:33:12 +0800

    添加项目管理接口

--- src/utils/api.js
+++ src/utils/api.js
@@ -24,3 +24,67 @@ export const getPauseProject = data => {
     data: qs.stringify(data),
   });
 };
+/* 项目列表 */
+export const getProjectList = data => {
+  return request({
+    method: 'POST',
+    url: '/Project/list',
+    data: qs.stringify(data),
+  });
+};
+/* 添加用户数据 */
+export const saveProjectData = data => {
+  return request({
+    method: 'POST',
+    url: '/project/add_project_data',
+    data: qs.stringify(data),
+  });
+};
+/* 项目审核 */
+export const examine = data => {
+  return request({
+    method: 'POST',
+    url: '/project/examine',
+    data: qs.stringify(data),
+  });
+};
+/* 用户项目列表 */
+export const getUserProjectList = data => {
+  return request({
+    method: 'POST',
+    url: '/project/user_project_list',
+    data: qs.stringify(data),
+  });
+};
+/* 用户申请列表 */
+export const getUserApplyList = data => {
+  return request({
+    method: 'POST',
+    url: '/project/user_apply_list',
+    data: qs.stringify(data),
+  });
+};
+/* 用户申请 */
+export const saveApply = data => {
+  return request({
+    method: 'POST',
+    url: '/project/apply',
+    data: qs.stringify(data),
+  });
+};
+/* 修改项目 */
+export const updateProject = data => {
+  return request({
+    method: 'POST',
+    url: '/Project/edit',
+    data: qs.stringify(data),
+  });
+};
+/* 添加项目 */
+export const saveProject = data => {
+  return request({
+    method: 'POST',
+    url: '/Project/add',
+    data: qs.stringify(data),
+  });
+};
--- src/views/project/apply-list.vue
+++ src/views/project/apply-list.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="app-page">
 
-    <Head :searchParams="templateParams" :functionParams="functionParams" />
+    <Head :searchParams="templateParams" :functionParams="functionParams" @searchList="doSearch" />
     <el-table v-loading="tableLoading" :data="tableData" :header-cell-style="_headerCellStyle" border
       element-loading-spinner="el-icon-loading" element-loading-text="加载中，请稍候……">
-      <el-table-column label="排序" type="index" align="center" width="100"></el-table-column>
-      <el-table-column label="项目名称" prop="name" align="center"></el-table-column>
-      <el-table-column label="推广类型" prop="name" align="center"></el-table-column>
+      <el-table-column label="排序" prop="id" align="center" width="100"></el-table-column>
+      <el-table-column label="项目名称" prop="title" align="center"></el-table-column>
+      <el-table-column label="推广类型" prop="" align="center"></el-table-column>
       <el-table-column label="单价（元）" prop="price" align="center"></el-table-column>
-      <el-table-column label="项目流程" prop="jiesuan" align="center">
+      <el-table-column label="项目流程" prop="" align="center">
         <template slot-scope="scope">
           <el-link type="primary" :underline="false">图片</el-link>
           <el-link type="primary" :underline="false">视频</el-link>
@@ -32,39 +32,15 @@
 <script>
 import Head from "@/components/Head/index.vue";
 import listMixin from "@/mixins/listMixin";
-import { PROMOTION_TYPE } from '@/utils/const'
+import { PROMOTION_TYPE } from "@/utils/const";
+import { getProjectList } from "@/utils/api";
 export default {
   components: { Head },
   mixins: [listMixin],
   data() {
     return {
       // 列表数据
-      tableData: [
-        {
-          date: "2022-1-11",
-          name: "龙珠",
-          data: "9999",
-          ciliu: "20%",
-          price: 1.0,
-          jiesuan: 999,
-        },
-        {
-          date: "2022-1-11",
-          name: "龙珠",
-          data: "9999",
-          ciliu: "20%",
-          price: 1.0,
-          jiesuan: 999,
-        },
-        {
-          date: "2022-1-11",
-          name: "龙珠",
-          data: "9999",
-          ciliu: "20%",
-          price: 1.0,
-          jiesuan: 999,
-        },
-      ],
+      tableData: [],
       // 搜索参数
       templateParams: [
         {
@@ -74,14 +50,14 @@ export default {
           placeholder: "请输入项目名称",
           type: "input",
         },
-        {
-          key: "date",
-          value: "",
-          label: "推广类型",
-          placeholder: "请选择",
-          type: "select",
-          data: PROMOTION_TYPE
-        },
+        // {
+        //   key: "date",
+        //   value: "",
+        //   label: "推广类型",
+        //   placeholder: "请选择",
+        //   type: "select",
+        //   data: PROMOTION_TYPE,
+        // },
       ],
       // 按钮参数
       functionParams: [{ text: "导出", callback: "exportListData" }],
@@ -90,8 +66,23 @@ export default {
   methods: {
     // 导出列表数据
     exportListData() {},
+    // 获取列表数据
+    fetchData() {
+      getProjectList(this.searchParams).then((res) => {
+        this.tableData = res.data.list;
+        this.total = res.data.num;
+      });
+    },
+    // 执行搜索
+    doSearch(params) {
+      this.searchParams = Object.assign(this.searchParams, params);
+      this.search();
+    },
   },
-  mounted() {},
+  mounted() {
+    // 获取列表数据
+    this.fetchData();
+  },
 };
 </script>
 
