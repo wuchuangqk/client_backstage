93b18c830490ab215f46b9d9e00e097346cb6590
[AnTanZhi] <[gaojiuan@163.com]>
2022-05-18 11:12:16 +0800

    资源文件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,14 @@
 <template>
-  <div>
-    
-  </div>
+  <div></div>
 </template>
 <script>
 export default {
   data() {
     return {};
   },
-  watch: {},
   methods: {},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
