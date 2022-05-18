2e926c07c83423fdff292d7e525699aae08d2705
[AnTanZhi] <[gaojiuan@163.com]>
2022-05-18 10:44:53 +0800

    更改检查风格

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,56 +1,16 @@
 <template>
   <div>
-    <el-button @click="handleLogin">登录</el-button>
+    
   </div>
 </template>
-
 <script>
 export default {
-  name: "Login",
   data() {
-    return {
-      redirect: undefined,
-      otherQuery: {},
-    };
+    return {};
   },
-  watch: {
-    $route: {
-      handler: function (route) {
-        const query = route.query;
-        if (query) {
-          this.redirect = query.redirect;
-          this.otherQuery = this.getOtherQuery(query);
-        }
-      },
-      immediate: true,
-    },
-  },
-  methods: {
-    handleLogin() {
-      this.$store
-        .dispatch("user/login")
-        .then(() => {
-          this.$router.push({
-            path: this.redirect || "/",
-            query: this.otherQuery,
-          });
-          this.loading = false;
-        })
-        .catch(() => {
-          this.loading = false;
-        });
-    },
-    getOtherQuery(query) {
-      return Object.keys(query).reduce((acc, cur) => {
-        if (cur !== "redirect") {
-          acc[cur] = query[cur];
-        }
-        return acc;
-      }, {});
-    },
-  },
+  watch: {},
+  methods: {},
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
