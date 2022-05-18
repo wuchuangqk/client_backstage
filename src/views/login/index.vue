57b8f64c223ddcd1c27f0c256802900378f7ad15
[AnTanZhi] <[gaojiuan@163.com]>
2022-05-18 13:40:19 +0800

    登录ui

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,23 @@
 <template>
-  <div></div>
+  <div class="content">
+    <div class="content_1">
+      <div class="content_1_1">蚂蚁管理后台登录</div>
+      <div class="content_1_2" />
+      <input type="text" class="content_1_3" placeholder="请输入账号">
+      <input type="password" class="content_1_3" placeholder="请输入密码">
+      <div class="content_1_4">登录</div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return { input: "" };
   },
   methods: {},
 };
 </script>
 
 <style lang="scss" scoped>
+@import "./index.scss";
 </style>
