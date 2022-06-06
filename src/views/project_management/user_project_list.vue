737f74dddafd5d0e3c4698281a83326c7d08f773
吴闯 <wuchuang456@126.com>
2022-06-06 10:35:35 +0800

    用户项目列表分页

--- src/views/project_management/user_project_list.vue
+++ src/views/project_management/user_project_list.vue
@@ -1,31 +1,36 @@
 <template>
-  <div>
-    <div class="content">
-      <el-table :data="tableData" style="width: 100%" border
-        :header-cell-style="{ background: '#F8FBFF', color: '#505050' }">
-        <el-table-column type="index" label="排序" width="50" align="center" />
-        <el-table-column prop="title" label="项目名" />
-        <el-table-column prop="add_time" label="添加时间" width="160" align="center" />
-        <el-table-column prop="user_name" label="做单人账号" />
-        <el-table-column prop="real_name" label="真实姓名" />
-        <el-table-column prop="city" label="地址" />
-        <el-table-column prop="phone" label="手机号" />
-        <el-table-column label="项目状态" width="80" align="center">
-          <template slot-scope="s">
-            <el-tag type="danger" v-if="s.row.status == 0">异常</el-tag>
-            <el-tag type="success" v-if="s.row.status == 1">正常</el-tag>
-            <el-tag v-if="s.row.status == 2">待审核</el-tag>
-            <el-tag type="warning" v-if="s.row.status == 3">暂停</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column label="审批" width="150" align="center">
-          <template slot-scope="s">
-            <el-button type="text" @click="saveProjectDataFront(s.row)">添加数据</el-button>
-            <el-button type="text" @click="doPause(s.row)">暂停</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </div>
+  <div class="app-page">
+
+    <Head :searchParams="templateParams" @searchList="doSearch" />
+    <el-table :data="tableData" style="width: 100%" border
+      :header-cell-style="{ background: '#F8FBFF', color: '#505050' }">
+      <el-table-column type="index" label="排序" width="50" align="center" />
+      <el-table-column prop="title" label="项目名" />
+      <el-table-column prop="add_time" label="添加时间" width="160" align="center" />
+      <el-table-column prop="user_name" label="做单人账号" />
+      <el-table-column prop="real_name" label="真实姓名" />
+      <el-table-column prop="city" label="地址" />
+      <el-table-column prop="phone" label="手机号" />
+      <el-table-column label="项目状态" width="80" align="center">
+        <template slot-scope="s">
+          <el-tag type="danger" v-if="s.row.status == 0">异常</el-tag>
+          <el-tag type="success" v-if="s.row.status == 1">正常</el-tag>
+          <el-tag v-if="s.row.status == 2">待审核</el-tag>
+          <el-tag type="warning" v-if="s.row.status == 3">暂停</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="审批" width="150" align="center">
+        <template slot-scope="s">
+          <el-button type="text" @click="saveProjectDataFront(s.row)">添加数据</el-button>
+          <el-button type="text" @click="doPause(s.row)">暂停</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <footer class="app-pagination-wrap">
+      <el-pagination :page-sizes="pageSizes" background layout="prev, pager, next, jumper" :total="total"
+        :page-size="searchParams.num" :current-page="searchParams.page" @size-change="handleSizeChange"
+        @current-change="handleCurrentChange" />
+    </footer>
     <el-dialog title="添加数据" :visible.sync="saveProjectDialog">
       <el-form :model="saveProjectParams" label-position="left" label-width="70px">
         <el-form-item label="日期">
@@ -54,26 +59,41 @@
 
 <script>
 import { getUserProjectList, saveProjectData, examine } from '@/utils/api'
+import listMixin from "@/mixins/listMixin";
+import Head from "@/components/Head/index.vue";
 export default {
+  mixins: [listMixin],
+  components: { Head },
   data() {
     return {
       tableData: [],
-      total: "",
-      params: {
-        page: 1,
-        num: 10,
-        status: 1,
-      },
       saveProjectDialog: false,
       saveProjectParams: {},
       pauseLoading: false,
+      // 搜索参数
+      templateParams: [
+        {
+          key: "id",
+          value: "",
+          label: "真实姓名",
+          placeholder: "请选择用户",
+          type: "select",
+          data: []
+        },
+      ],
     }
   },
   methods: {
-    userProjectList() {
-      getUserProjectList(this.params).then(res => {
+    fetchData() {
+      getUserProjectList(this.searchParams).then(res => {
         this.tableData = res.data.list
-        this.total = res.data.total
+        this.total = res.data.num
+        this.templateParams[0].data = (res.data.select || []).map(v => {
+          return {
+            key: v.real_name,
+            value: v.id
+          }
+        })
       })
     },
     saveProjectData() {
@@ -81,7 +101,7 @@ export default {
         if (res.code === -1) return this.$message.error(res.msg)
         this.$message.success('添加成功')
         this.saveProjectDialog = false
-        this.userProjectList()
+        this.fetchData()
       })
     },
     saveProjectDataFront(item) {
@@ -104,16 +124,25 @@ export default {
         this.pauseLoading = false
         if (res.code != 1) return this.$message.error(res.msg);
         this.$message.success('暂停成功');
-        this.userProjectList()
+        this.fetchData()
       })
-    }
+    },
+    // 执行搜索
+    doSearch(params) {
+      this.searchParams = Object.assign(this.searchParams, params);
+      this.search();
+    },
   },
   mounted() {
-    this.userProjectList()
+    this.fetchData()
   },
 }
 </script>
 
 <style lang="scss" scoped>
 @import './user_project_list.scss';
+
+::v-deep .header {
+  padding: 0;
+}
 </style>
